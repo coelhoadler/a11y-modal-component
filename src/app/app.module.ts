b3fd34d1e96186cae2195dd6fb80db4ab8e4dcbd
Adler Santos <adler.coelho.santos@icloud.com>
2023-12-29 13:43:12 -0300

    refactor(modal): estilizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from './shared/modal/modal.module';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
